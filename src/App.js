c8115f29bd4b10560b38b4bc7664334a90abf5e4
KOLAWOLE OGUNFOWOKAN <kolawole.ogunfowokan@konga.com>
2021-02-21 21:11:45 +0100

    update app with shop page, collection item and preview component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 import HomePage from './pages/homepage/HomePage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import ShopPage from './pages/shoppage/shop.component'
 
 function App() {
   return (
@@ -16,7 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
