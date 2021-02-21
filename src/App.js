598c1ed503e003752befe358c3b2f0ba8fe47538
KOLAWOLE OGUNFOWOKAN <kolawole.ogunfowokan@konga.com>
2021-02-21 22:08:02 +0100

    add header components and link to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/HomePage.component';
 import ShopPage from './pages/shoppage/shop.component'
+import Header from './components/header/Header.component'
 
 function App() {
   return (
 
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
